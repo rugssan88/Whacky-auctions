2f205b4f59c38b3c31c4d1cb9fdfd550c830d653
rugssan88 <rugs.san88@gmail.com>
2026-09-20 19:34:00 +0200

    Add Whacky Auctions brand logo and app icons

--- public/sw.js
+++ public/sw.js
@@ -1,5 +1,5 @@
 const CACHE='whacky-v6-first-100-funnel-v8-seo-production-assets';
-const APP_SHELL=['/','/index.html','/styles.min.css','/app.min.js','/manifest.webmanifest','/icons/icon-192.png','/icons/icon-512.png'];
+const APP_SHELL=['/','/index.html','/styles.min.css','/app.min.js','/manifest.webmanifest','/branding/whacky-auctions-logo.png','/icons/favicon-64.png','/icons/apple-touch-icon.png','/icons/icon-192.png','/icons/icon-512.png'];
 self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(APP_SHELL)).then(()=>self.skipWaiting())));
 self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
 self.addEventListener('fetch',e=>{
